567db70813e993860b9c2da5528775eea7e5016e
aminogira <aminogira@gmail.com>
2023-01-24 09:32:54 +0530

    permutations

--- hr/basic/itertools_permutations.py
+++ hr/basic/itertools_permutations.py
@@ -14,6 +14,7 @@ from itertools import permutations
 # [('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
 
 
+
 from itertools import permutations
 inList=[]
 inList = input().split(' ')
